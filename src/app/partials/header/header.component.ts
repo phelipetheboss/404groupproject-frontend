798ff559efff6ac0979f3dbcc2762c5a724bcbf4
Dudu Missono <dudumissono@gmail.com>
2022-11-25 23:52:49 -0500

    auth logic implemented

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Router } from "@angular/router";
+import { AuthService } from "../../admin/auth/auth.service";
+
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,14 @@ import { Router } from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(public authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   logout(){
-    this.router.navigate(['/home'])
+    this.authService.clearStorage()
+    this.authService.logoutUser()
+    this.router.navigate([''])
   }
-
 }
